1a78e6452c6ce6df8b71f38649311a616655b013
CarlosJaBR <carjabo12@gmail.com>
2025-02-23 14:34:57 -0500

    fix: change class name

--- src/main/java/com/members/membersService/repository/MemberRepository.java
+++ src/main/java/com/members/membersService/repository/MemberRepository.java
@@ -3,4 +3,4 @@ package com.members.membersService.repository;
 import com.members.membersService.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberService extends JpaRepository<Member, Long> {}
+public interface MemberRepository extends JpaRepository<Member, Long> {}
