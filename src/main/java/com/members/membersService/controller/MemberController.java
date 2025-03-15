8cbc1a8d2fa9133223c3b7b37c8a69e4b3ba625a
CarlosJaBR <carjabo12@gmail.com>
2025-03-15 15:50:24 -0500

    add keycloak and jwt configuration

--- src/main/java/com/members/membersService/controller/MemberController.java
+++ src/main/java/com/members/membersService/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.members.membersService.controller;
 import com.members.membersService.model.Member;
 import com.members.membersService.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +15,13 @@ public class MemberController {
     @Autowired
     private MemberService memberService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("")
     public Member addMember(@RequestBody Member member) {
         return memberService.registerMember(member);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("")
     public List<Member> getAllMembers() {
         return memberService.getAllMembers();
