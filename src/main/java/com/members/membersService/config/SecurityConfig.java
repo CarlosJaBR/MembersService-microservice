ac746ef819041f478b320625b40a78081caf931d
CarlosJaBR <carjabo12@gmail.com>
2025-04-21 15:40:17 -0500

    add eureka

--- src/main/java/com/members/membersService/config/SecurityConfig.java
+++ src/main/java/com/members/membersService/config/SecurityConfig.java
@@ -2,14 +2,11 @@ package com.members.membersService.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,11 +29,6 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                 )
-                .oauth2ResourceServer(oauth2 ->
-                        oauth2.jwt(jwt ->
-                                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
-                        )
-                )
                 .csrf(csrf ->
                         csrf.ignoringRequestMatchers(
                                 "/swagger-ui/**",
@@ -48,16 +40,5 @@ public class SecurityConfig {
                 );
         return http.build();
     }
-
-    private Converter<
-            Jwt,
-            ? extends AbstractAuthenticationToken
-            > jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtConverter =
-                new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(
-                new KeycloakRealmRoleConverter()
-        );
-        return jwtConverter;
-    }
 }
+
